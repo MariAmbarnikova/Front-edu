782c494b856eb82cb36cf8f234e46435c6368be4
Marina <ambarnikovama@gmail.com>
2023-10-26 16:21:59 +0300

    initial commit

---
+++ 01-variables/lesson/index.js
@@ -0,0 +1,47 @@
+const myName = 'Marina';
+const myAge = 34;
+const boolean = true;
+console.log(boolean);
+let randomNumber = 4;
+let randomStr = 'рандомный текст';
+let num = null;
+let variable
+const obj = {};
+const HelloWorld = function () {
+    console.log('Hello World!');
+};
+
+
+let a = 5;
+let b = 6;
+if (a >= b) {
+    console.log(a);
+} else if (a < b) {
+    console.log(b);
+}
+
+const str1 = 'Hello';
+const str2 = 'World';
+console.log(str1 === str2);
+
+console.log(a == str1);
+
+const boolean1 = true;
+const boolean2 = true;
+
+console.log(boolean1 === boolean2);
+
+console.log(a === boolean1);
+
+console.log(str1 === boolean1);
+
+console.log(a === num);
+
+console.log(variable == num);
+console.log(variable === num);
+
+const ten = 10;
+const hundred = 100;
+const thousand = 1000;
+
+console.log((ten * ten + hundred) * hundred >= thousand * ten)
